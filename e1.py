3ff182ec356595d7018a2213a8fb727741a13ac3
崔晏宁 <cuiyanning@cuiyanningdeMacBook-Pro.local>
2016-11-08 00:08:15 -0600

    e1.py done

---
+++ e1.py
@@ -0,0 +1,12 @@
+import requests
+from bs4 import BeautifulSoup as bs
+resp=requests.get("http://historical.elections.virginia.gov/elections/search/year_from:1924/year_to:2015/office_id:1/stage:General")
+soup = bs(resp.content, "html.parser")
+resp.content
+l = soup.find_all("tr", "election_item")
+with open("ELECTION_ID", "w") as out:
+  for x in l:
+    el_id = int(x["id"].split("-")[-1])
+    year = int(x.find("td", "year").string)
+    print(year,el_id)
+    out.write("{} {}".format(year, el_id))
